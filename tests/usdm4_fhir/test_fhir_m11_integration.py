e7e0e484b55b649f3e1a33fc60fa0fec76f4c5fd
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2025-09-05 14:44:03 +0200

    Improve tests

--- tests/usdm4_fhir/test_fhir_m11_integration.py
+++ tests/usdm4_fhir/test_fhir_m11_integration.py
@@ -1,6 +1,7 @@
 import json
-from tests.usdm4_fhir.files.files import read_yaml, write_json, read_json
+from tests.usdm4_fhir.helpers.files import read_yaml, write_json, read_json, write_yaml
 from tests.usdm4_fhir.helpers.helpers import fix_uuid, fix_iso_dates
+from tests.usdm4_fhir.helpers.errors_clean import errors_clean_all
 from usdm4_fhir import M11
 from usdm4 import USDM4
 
@@ -32,16 +33,36 @@ def run_test(name, version, mode, save=False):
     result = fix_uuid(result)
     pretty_result = json.dumps(json.loads(result), indent=2)
     result_filename = f"{name}_fhir_m11.json"
+    error_filename = f"{name}_errors.yaml"
     if save:
         write_json(_full_path(result_filename, version, mode), result)
+        write_yaml(_full_path(name, error_filename), errors_clean_all(instance.errors))
     expected = read_json(_full_path(result_filename, version, mode))
     assert pretty_result == expected
+    error_expected = read_yaml(_full_path(name, error_filename))
+    assert errors_clean_all(instance.errors) == error_expected
 
 def _full_path(filename, version, mode):
     return f"tests/usdm4_fhir/test_files/m11/{mode}/{version}/{filename}"
 
 
 def test_to_fhir_prism2_asp8062():
+    run_test_to_prism2("ASP8062", SAVE)
+
+
+def test_to_fhir_prism2_deucralip():
+    run_test_to_prism2("DEUCRALIP", SAVE)
+
+
+def test_to_fhir_prism2_igbj():
+    run_test_to_prism2("IGBJ", SAVE)
+
+
+def test_to_fhir_prism2_deucralip():
+    run_test_to_prism2("pilot", SAVE)
+
+
+def test_to_fhir_madrid_asp8062():
     run_test_to_madrid("ASP8062", SAVE)
 
 
@@ -51,7 +72,3 @@ def test_to_fhir_madrid_igbj():
 
 def test_to_fhir_madrid_pilot():
     run_test_to_madrid("pilot", SAVE)
-
-
-def test_to_fhir_madrid_asp8062():
-    run_test_to_madrid("ASP8062", SAVE)

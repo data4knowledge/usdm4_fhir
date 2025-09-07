ec2ff152e0de8dd1ea8582c99607a603123fa1f9
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2025-09-07 19:06:23 +0200

    Adding features

--- docs/notes.md
+++ docs/notes.md
@@ -1,4 +1,73 @@
-# ResearchStudy
-## id
-- What should this be being set to?
-- Setting to sponsior identifier plus "-ResearchStudy"
+# M11 Export
+
+## ResearchStudy
+
+### id
+What should this be being set to? Currently setting to sponsor identifier plus "-ResearchStudy"
+
+### meta
+Fixed and set to
+```
+{
+    "profile": [
+        "http://hl7.org/fhir/uv/pharmaceutical-research-protocol/StructureDefinition/m11-research-study-profile"
+    ]
+}
+```
+
+### text
+What is this all about?
+
+### identifiers
+Set to list of available study identidiers
+
+The following parameters look "interesting":
+- "use"
+- "type"
+- "system"
+
+### version
+Setting to USDM version
+
+### title
+Set to offical title
+
+### label
+Study titles acronym and short title
+
+### date
+Using approval date
+
+### status
+Document status, hard coded to first document 
+
+### phase
+Code system set to thresarus owl file?
+We also have to take into account xM11 preferred terms 
+
+### focus
+
+TBD
+
+### condition
+
+TBD
+
+### extensions
+
+#### 
+
+#### confidentiality 
+
+```
+{
+    "url": "http://hl7.org/fhir/uv/ebm/StructureDefinition/research-study-sponsor-confidentiality-statement",
+    "valueString": "All data is confidential"
+}
+```
+
+
+
+
+
+
--- src/usdm4_fhir/factory/research_study_factory_p3.py
+++ src/usdm4_fhir/factory/research_study_factory_p3.py
@@ -2,6 +2,7 @@ import datetime
 from usdm4.api.study import Study as USDMStudy
 from usdm4.api.study_version import StudyVersion as USDMStudyVersion
 from usdm4.api.governance_date import GovernanceDate
+from usdm4.api.study_title import StudyTitle
 from fhir.resources.researchstudy import ResearchStudy
 from usdm4_fhir.factory.base_factory import BaseFactory
 from usdm4_fhir.factory.extension_factory import ExtensionFactory
@@ -21,6 +22,7 @@ class ResearchStudyFactoryP3(BaseFactory):
             # self._amendment = extra['amendment']
             self._version: USDMStudyVersion = study.first_version()
             self._study_design = self._version.studyDesigns[0]
+            self._document = study.documentedBy[0].versions[0]
             self._organizations: dict = self._version.organization_map()
 
             # Set Profile meta data
@@ -45,30 +47,26 @@ class ResearchStudyFactoryP3(BaseFactory):
                 outcomeMeasure=[],
                 protocol=[],
             )
-
-            # Sponsor Confidentiality Statememt
-            if self._title_page["sponsor_confidentiality"]:
-                ext = ExtensionFactory(
-                    **{
-                        "url": "http://hl7.org/fhir/uv/ebm/StructureDefinition/research-study-sponsor-confidentiality-statement",
-                        "valueString": self._title_page["sponsor_confidentiality"],
-                    }
-                )
-                self.item.extension.append(ext.item)
-
+    
             # Full Title
             self.item.title = (
                 self._version.official_title_text()
-            )  # self._get_title('Official Study Title').text
+            ) 
 
-            # Trial Acronym
-            acronym = self._version.acronym()  # self._get_title('Study Acronym')
+            # Trial Acronym and Short Title
+            acronym: StudyTitle = self._version.acronym()
             if acronym:
                 self.item.label.append(
                     LabelTypeFactory(usdm_code=acronym.type, text=acronym.text).item
                 )
+            st: StudyTitle = self._version.short_title()
+            if st:
+                self.item.label.append(
+                    LabelTypeFactory(usdm_code=st.type, text=st.text).item
+                )
 
-            # Sponsor Protocol Identifier
+
+            # Identifiers
             for identifier in self._version.studyIdentifiers:
                 org = identifier.scoped_by(self._organizations)
                 identifier_cc = CodeableConceptFactory(text=org.type.decode)
@@ -95,15 +93,15 @@ class ResearchStudyFactoryP3(BaseFactory):
             if date_value:
                 self.item.date = date_value
 
-            # Amendment Identifier
-            identifier_code = CodeableConceptFactory(text="Amendment Identifier")
-            self.item.identifier.append(
-                {
-                    "type": identifier_code.item,
-                    "system": "https://example.org/amendment-identifier",
-                    "value": self._title_page["amendment_identifier"],
-                }
-            )
+            # # Amendment Identifier
+            # identifier_code = CodeableConceptFactory(text="Amendment Identifier")
+            # self.item.identifier.append(
+            #     {
+            #         "type": identifier_code.item,
+            #         "system": "https://example.org/amendment-identifier",
+            #         "value": self._title_page["amendment_identifier"],
+            #     }
+            # )
 
             # Amendment Scope - Part of Amendment
 
@@ -116,8 +114,7 @@ class ResearchStudyFactoryP3(BaseFactory):
             # Trial Phase
             phase = self._study_design.phase()
             phase_code = CodingFactory(
-                system=phase.codeSystem,
-                version=phase.codeSystemVersion,
+                system="http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl",
                 code=phase.code,
                 display=phase.decode,
             )
@@ -125,23 +122,28 @@ class ResearchStudyFactoryP3(BaseFactory):
                 coding=[phase_code.item], text=phase.decode
             ).item
 
-            # Short Title
-            title = self._version.short_title()  # self._get_title('Brief Study Title')
-            self.item.label.append(
-                LabelTypeFactory(usdm_code=title.type, text=title.text).item
-            )
+            # Status
+            self.item.status = self._document.status.decode
 
-            # Sponsor Name and Address
-            sponsor = self._version.sponsor()
-            print(f"SPONSOR: {sponsor}")
-            org = OrganizationFactory(sponsor)
-            # self._entries.append({'item': org.item, 'url': 'https://www.example.com/Composition/1234D'})
-            ap = AssociatedPartyFactory(
-                party={"reference": f"Organization/{self.fix_id(org.item.id)}"},
-                role_code="sponsor",
-                role_display="sponsor",
-            )
-            self.item.associatedParty.append(ap.item)
+            # Sponsor Confidentiality Statememt
+            if self._title_page["sponsor_confidentiality"]:
+                ext = ExtensionFactory(
+                    **{
+                        "url": "http://hl7.org/fhir/uv/ebm/StructureDefinition/research-study-sponsor-confidentiality-statement",
+                        "valueString": self._title_page["sponsor_confidentiality"],
+                    }
+                )
+                self.item.extension.append(ext.item)
+
+            # # Sponsor Name and Address
+            # sponsor = self._version.sponsor()
+            # org = OrganizationFactory(sponsor)
+            # ap = AssociatedPartyFactory(
+            #     party={"reference": f"Organization/{self.fix_id(org.item.id)}"},
+            #     role_code="sponsor",
+            #     role_display="sponsor",
+            # )
+            # self.item.associatedParty.append(ap.item)
 
             # Manufacturer Name and Address
             # x = self._title_page['manufacturer_name_and_address']
@@ -149,19 +151,19 @@ class ResearchStudyFactoryP3(BaseFactory):
             # Regulatory Agency Identifiers, see above
             # x = self._title_page['regulatory_agency_identifiers']
 
-            # Sponsor Approval
-            g_date: GovernanceDate = self._version.approval_date()
-            date_str = (
-                g_date.dateValue
-                if g_date
-                else datetime.datetime.now(tz=datetime.timezone.utc).isoformat()
-            )
-            status = ProgressStatusFactory(
-                value=date_str,
-                state_code="sponsor-approved",
-                state_display="sponsor apporval date",
-            )
-            self.item.progressStatus.append(status.item)
+            # # Sponsor Approval
+            # g_date: GovernanceDate = self._version.approval_date()
+            # date_str = (
+            #     g_date.dateValue
+            #     if g_date
+            #     else datetime.datetime.now(tz=datetime.timezone.utc).isoformat()
+            # )
+            # status = ProgressStatusFactory(
+            #     value=date_str,
+            #     state_code="sponsor-approved",
+            #     state_display="sponsor apporval date",
+            # )
+            # self.item.progressStatus.append(status.item)
 
             # # Sponsor Signatory
             # ap = AssociatedPartyFactory(party={'value': self._title_page['sponsor_signatory']}, role_code='sponsor-signatory', role_display='sponsor signatory')

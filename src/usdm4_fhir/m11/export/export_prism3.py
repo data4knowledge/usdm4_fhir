2b9b45b2fccf079767b1597e273f0df926f7db27
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2026-01-14 19:43:57 +0100

    Few more tweaks to align with spec

--- src/usdm4_fhir/m11/export/export_prism3.py
+++ src/usdm4_fhir/m11/export/export_prism3.py
@@ -10,6 +10,7 @@ from usdm4_fhir.factory.composition_factory import CompositionFactory
 from usdm4_fhir.factory.extension_factory import ExtensionFactory
 from fhir.resources.bundle import Bundle, BundleEntry
 from usdm4_fhir.factory.group_factory import GroupFactory
+from usdm4_fhir.factory.coding_factory import CodingFactory
 
 
 class ExportPRISM3(ExportBase):
@@ -117,7 +118,13 @@ class ExportPRISM3(ExportBase):
     ):
         section = self._content_to_section(content, processed_map)
         if section:
-            type_code = CodeableConceptFactory(text="EvidenceReport").item
+            # type_code = CodeableConceptFactory(text="EvidenceReport").item
+            code = CodingFactory(
+                system="http://hl7.org/fhir/research-study-party-role",
+                code="b001",
+                display="Protocol narative",
+            )
+            type_code = CodeableConceptFactory(coding=[code.item])
             author = ReferenceFactory(display="USDM").item
             return CompositionFactory(
                 title=section.title,
@@ -167,7 +174,7 @@ class ExportPRISM3(ExportBase):
             if text:
                 outer: ExtensionFactory = ExtensionFactory(
                     **{
-                        "url": "http://hl7.org/fhir/6.0/StructureDefinition/extension-Group.description",
+                        "url": "http://hl7.org/fhir/6.0/StructureDefinition/extension-Group.characteristic.description",
                         "valueString": str(text),
                     }
                 )
@@ -190,11 +197,153 @@ class ExportPRISM3(ExportBase):
                     f"Criterion item with id '{criterion_item.id}' has empty text, text '{criterion_item.text}' -translated-> '{text}'"
                 )
 
-    # # First cut of amendment code
-    # def _amendment_ext(self):
-    #     if len(self.study_version.amendments) == 0:
-    #         return None
-    #     source = self.study_version.amendments[0]
+    # First cut of amendment code. Example structure
+    # ==============================================
+    #
+    # {
+    #   "extension" : [
+    #     {
+    #       "url" : "scope",
+    #       "valueCode" : "C217026"
+    #     },
+    #     {
+    #       "url" : "country",
+    #       "valueCode" : "DE"
+    #     },
+    #     {
+    #       "url" : "country",
+    #       "valueCode" : "GB"
+    #     },
+    #     {
+    #       "url" : "region",
+    #       "valueCode" : "AU-NSW"
+    #     },
+    #     {
+    #       "url" : "site",
+    #       "valueIdentifier" : {
+    #         "system" : "https://example.org/site-identifier",
+    #         "value" : "sss"
+    #       }
+    #     },
+    #     {
+    #       "url" : "approvalDate",
+    #       "valueDate" : "2017-12-05"
+    #     },
+    #     {
+    #       "url" : "signature",
+    #       "valueSignature" : {
+    #         "data" : "PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiPz4KPEVudmVsb3BlIHhtbG5zPSJ1cm46ZW52ZWxvcGUiPgogIDxTaWduYXR1cmUgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvMDkveG1sZHNpZyMiPgogICAgPFNpZ25lZEluZm8+CiAgICAgIDxDYW5vbmljYWxpemF0aW9uTWV0aG9kIAogICAgICAgICAgIEFsZ29yaXRobT0iaHR0cDovL3d3dy53My5vcmcvVFIvMjAwMS9SRUMteG1sLWMxNG4tCjIwMDEwMzE1I1dpdGhDb21tZW50cyIvPgogICAgICA8U2lnbmF0dXJlTWV0aG9kIEFsZ29yaXRobT0iaHR0cDovL3d3dy53My5vcmcvMjAwMC8wOS8KeG1sZHNpZyNkc2Etc2hhMSIvPgogICAgICA8UmVmZXJlbmNlIFVSST0iIj4KICAgICAgICA8VHJhbnNmb3Jtcz4KICAgICAgICAgIDxUcmFuc2Zvcm0gQWxnb3JpdGhtPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwLzA5Lwp4bWxkc2lnI2VudmVsb3BlZC1zaWduYXR1cmUiLz4KICAgICAgICA8L1RyYW5zZm9ybXM+CiAgICAgICAgPERpZ2VzdE1ldGhvZCBBbGdvcml0aG09Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvMDkvCnhtbGRzaWcjc2hhMSIvPgogICAgICAgIDxEaWdlc3RWYWx1ZT51b29xYldZYTVWQ3FjSkNidXltQktxbTE3dlk9PC9EaWdlc3RWYWx1ZT4KICAgICAgPC9SZWZlcmVuY2U+CiAgICA8L1NpZ25lZEluZm8+CjxTaWduYXR1cmVWYWx1ZT4KS2VkSnVUb2I1Z3R2WXg5cU0zazNnbTdrYkxCd1ZiRVFSbDI2UzJ0bVhqcU5ORDdNUkd0b2V3PT0KICAgIDwvU2lnbmF0dXJlVmFsdWU+CiAgICA8S2V5SW5mbz4KICAgICAgPEtleVZhbHVlPgogICAgICAgIDxEU0FLZXlWYWx1ZT4KICAgICAgICAgIDxQPgovS2FDem80U3lyb203OHozRVE1U2JiQjRzRjdleTgwZXRLSUk4NjRXRjY0QjgxdVJwSDV0OWpRVHhlCkV1MEltYnpSTXF6VkRaa1ZHOXhEN25OMWt1Rnc9PQogICAgICAgICAgPC9QPgogICAgICAgICAgPFE+bGk3ZHpEYWN1bzY3Smc3bXRxRW0yVFJ1T01VPTwvUT4KICAgICAgICAgIDxHPlo0UnhzbnFjOUU3cEdrbkZGSDJ4cWFyeVJQQmFRMDFraHBNZExSUW5HNTQxQXd0eC8KWFBhRjVCcHN5NHBOV01PSENCaU5VME5vZ3BzUVc1UXZubE1wQT09CiAgICAgICAgICA8L0c+CiAgICAgICAgICA8WT5xVjM4SXFyV0pHMFYvCm1aUXZSVmkxT0h3OVpqODRuREM0ak84UDBheGkxZ2I2ZCs0NzV5aE1qU2MvCkJySVZDNThXM3lkYmtLK1JpNE9LYmFSWmxZZVJBPT0KICAgICAgICAgPC9ZPgogICAgICAgIDwvRFNBS2V5VmFsdWU+CiAgICAgIDwvS2V5VmFsdWU+CiAgICA8L0tleUluZm8+CiAgPC9TaWduYXR1cmU+CjwvRW52ZWxvcGU+IA=="
+    #       }
+    #     },
+    #     {
+    #       "url" : "signatureUrl",
+    #       "valueUrl" : "https://somelocation"
+    #     },
+    #     {
+    #       "url" : "signatureMethod",
+    #       "valueString" : "electronic and wet ink copy"
+    #     },
+    #     {
+    #       "extension" : [
+    #         {
+    #           "url" : "scope",
+    #           "valueCode" : "C41065"
+    #         },
+    #         {
+    #           "url" : "number",
+    #           "valuePositiveInt" : 234
+    #         }
+    #       ],
+    #       "url" : "http://hl7.org/fhir/uv/clinical-study-protocol/StructureDefinition/ResearchStudyStudyAmendmentScopeImpact"
+    #     },
+    #     {
+    #       "extension" : [
+    #         {
+    #           "url" : "scope",
+    #           "valueCode" : "C68846"
+    #         },
+    #         {
+    #           "url" : "number",
+    #           "valuePositiveInt" : 983
+    #         }
+    #       ],
+    #       "url" : "http://hl7.org/fhir/uv/clinical-study-protocol/StructureDefinition/ResearchStudyStudyAmendmentScopeImpact"
+    #     },
+        # {
+        #   "url" : "primaryReason",
+        #   "valueCodeableConcept" : {
+        #     "coding" : [
+        #       {
+        #         "system" : "http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl",
+        #         "code" : "C218490",
+        #         "display" : "Regulatory Agency Request To Amend"
+        #       }
+        #     ]
+        #   }
+        # },
+        # {
+        #   "url" : "secondaryReason",
+        #   "valueCodeableConcept" : {
+        #     "coding" : [
+        #       {
+        #         "system" : "http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl",
+        #         "code" : "C218494",
+        #         "display" : "Manufacturing Change"
+        #       }
+        #     ]
+        #   }
+        # },
+        # {
+        #   "url" : "summary",
+        #   "valueString" : "Regulator required manufacturing chanage."
+        # },
+        # {
+        #   "url" : "substantialImpactSafety",
+        #   "valueCode" : "C49488"
+        # },
+        # {
+        #   "url" : "substantialImpactSafetyComment",
+        #   "valueString" : "Specifically implemented to decrease safety risks."
+        # },
+        # {
+        #   "url" : "substantialImpactReliability",
+        #   "valueCode" : "C17998"
+        # },
+        # {
+        #   "url" : "substantialImpactReliabilityComment",
+        #   "valueString" : "ccc"
+        # },
+        # {
+        #   "extension" : [
+        #     {
+        #       "url" : "detail",
+        #       "valueString" : "amendment one"
+        #     },
+        #     {
+        #       "url" : "rationale",
+        #       "valueString" : "clarification"
+        #     },
+        #     {
+        #       "url" : "section",
+        #       "valueCodeableConcept" : {
+        #         "coding" : [
+        #           {
+        #             "system" : "http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl",
+        #             "code" : "C218515",
+        #             "display" : "1.1 Protocol Synopsis"
+        #           }
+        #         ]
+        #       }
+        #     }
+        #   ],
+        #   "url" : "http://hl7.org/fhir/uv/clinical-study-protocol/StructureDefinition/protocol-amendment-detail"
+        # },
+    
+    def _amendment_ext(self):
+        if len(self.study_version.amendments) == 0:
+            return None
+        source = self.study_version.amendments[0]
+
     #     amendment: ExtensionFactory = ExtensionFactory(
     #         url="http://example.org/fhir/extension/studyAmendment", extension=[]
     #     )
